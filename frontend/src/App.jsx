8813e741409243976c22644f0d542df0863e0607
Maojin-juan <edwardelric199887@gmail.com>
2025-01-02 16:58:57 +0800

    完成 預先創建各路徑及頁面

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/adminView/layout";
+import AdminDashboard from "./pages/adminView/dashboard";
+import AdminFeatures from "./pages/adminView/features";
+import AdminOrders from "./pages/adminView/orders";
+import AdminProducts from "./pages/adminView/products";
+import ShoppingLayout from "./components/shoppingView/layout";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
@@ -14,6 +21,14 @@ function App() {
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+        <Route path="/shop" element={<ShoppingLayout />}></Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/notFound/index.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>page doesn&#39;t exist</div>;
+}
+
+export default NotFound;
