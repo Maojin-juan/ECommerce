66eff85528806ae206af2525458f2257274ec974
Maojin-juan <edwardelric199887@gmail.com>
2025-01-10 17:10:06 +0800

    完成 側邊欄及導航欄

--- frontend/src/components/adminView/sidebar.jsx
+++ frontend/src/components/adminView/sidebar.jsx
@@ -1,5 +1,23 @@
+import { ChartNoAxesCombined } from "lucide-react";
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+
 function AdminSidebar() {
-  return <div>admin sidebar</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Fragment>
+      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
+        <div
+          onClick={() => navigate("/admin/dashboard")}
+          className="flex cursor-pointer items-center gap-2"
+        >
+          <ChartNoAxesCombined size={30} />
+          <h1 className="text-xl font-extrabold">Admin Panel</h1>
+        </div>
+      </aside>
+    </Fragment>
+  );
 }
 
 export default AdminSidebar;
