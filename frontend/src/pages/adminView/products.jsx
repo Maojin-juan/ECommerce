5fb974cc694bb06490fe86506e0f045982745cb0
Maojin-juan <edwardelric199887@gmail.com>
2025-01-13 16:59:14 +0800

    未完成 製作 products 的 Sheet

--- frontend/src/pages/adminView/products.jsx
+++ frontend/src/pages/adminView/products.jsx
@@ -1,5 +1,62 @@
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import { Fragment, useState } from "react";
+
+function onSubmit() {}
+
+const initialFormData = {
+  imageL: null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+};
+
 function AdminProducts() {
-  return <div>admin products</div>;
+  const [openCreateProductDialog, setOpenCreateProductDialog] = useState(false);
+  const [formData, setFormData] = useState(initialFormData);
+
+  return (
+    <Fragment>
+      <div className="mb-5 flex w-full justify-end">
+        <Button onClick={() => setOpenCreateProductDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductDialog}
+        onOpenChange={() => {
+          setOpenCreateProductDialog(false);
+        }}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+          <div className="py-6">
+            <CommonForm
+              onSubmit={onSubmit}
+              formData={formData}
+              setFormData={setFormData}
+              buttonText="Add"
+              formControls={addProductFormElements}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </Fragment>
+  );
 }
 
 export default AdminProducts;
