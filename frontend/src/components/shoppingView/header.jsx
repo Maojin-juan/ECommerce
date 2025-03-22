6bcf33028ab73f6129e90202e8d6708e9db685f5
Maojin-juan <edwardelric199887@gmail.com>
2025-03-22 21:54:26 +0800

    feat: 實作購物車功能
    - 新增購物車UI元件，包括購物車側邊欄
    - 整合Redux購物車狀態管理
    - 實作購物車項目增減、刪除功能
    - 顯示購物車總金額及商品細節
    - 優化購物體驗與使用者互動

--- frontend/src/components/shoppingView/header.jsx
+++ frontend/src/components/shoppingView/header.jsx
@@ -14,6 +14,9 @@ import {
   DropdownMenuItem,
 } from "../ui/dropdown-menu";
 import { logoutUser } from "@/store/authSlice";
+import UserCartWrapper from "./cartWrapper";
+import { useEffect, useState } from "react";
+import { fetchCartItems } from "@/store/shop/cartSlice";
 
 function MenuItems() {
   return (
@@ -33,6 +36,8 @@ function MenuItems() {
 
 function HeaderRightContent() {
   const { user } = useSelector((state) => state.auth);
+  const { cartItems } = useSelector((state) => state.shopCart);
+  const [openCartSheet, setOpenCartSheet] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -40,12 +45,31 @@ function HeaderRightContent() {
     dispatch(logoutUser());
   }
 
+  useEffect(() => {
+    dispatch(fetchCartItems(user?.id));
+  }, [dispatch]);
+
+  console.log(cartItems);
+
   return (
     <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
-      <Button variant="outline" size="icon">
-        <ShoppingCart className="size-6" />
-        <span className="sr-only">User cart</span>
-      </Button>
+      <Sheet open={openCartSheet} onOpenChange={() => setOpenCartSheet(false)}>
+        <Button
+          onClick={() => setOpenCartSheet(true)}
+          variant="outline"
+          size="icon"
+        >
+          <ShoppingCart className="size-6" />
+          <span className="sr-only">User cart</span>
+        </Button>
+        <UserCartWrapper
+          cartItems={
+            cartItems && cartItems.items && cartItems.items.length > 0
+              ? cartItems.items
+              : []
+          }
+        />
+      </Sheet>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Avatar className="bg-black">
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import adminProductsSlice from "./admin/productsSlice";
 import shopProductsSlice from "./shop/productsSlice";
+import shopCartSlice from "./shop/cartSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductsSlice,
     shopProducts: shopProductsSlice,
+    shopCart: shopCartSlice,
   },
 });
 
