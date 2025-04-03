9f028a98f6445f8981e4b1743f723d3d8e53796c
Maojin-juan <edwardelric199887@gmail.com>
2025-04-03 16:24:53 +0800

    feat(frontend): 整合購物車與訂單系統
    - 購物車狀態與搜尋結果同步
    - 優化購物車商品顯示邏輯
    - 完善訂單處理控制器 (支付、庫存管理)

--- frontend/src/components/shoppingView/cartItemsContent.jsx
+++ frontend/src/components/shoppingView/cartItemsContent.jsx
@@ -6,10 +6,43 @@ import { useToast } from "@/hooks/use-toast";
 
 function UserCartItemsContent({ cartItem }) {
   const { user } = useSelector((state) => state.auth);
+  const { cartItems } = useSelector((state) => state.shopCart);
+  const { productList } = useSelector((state) => state.shopProducts);
   const dispatch = useDispatch();
   const { toast } = useToast();
 
   function handleUpdateQuantity(getCartItem, typeOfAction) {
+    if (typeOfAction === "plus") {
+      let getCartItems = cartItems.items || [];
+
+      if (getCartItems.length) {
+        // 當前操作的項目在購物車中的位置
+        const indexOfCurrentCartItem = getCartItems.findIndex(
+          (item) => item.productId === getCartItem?.productId,
+        );
+
+        // 當前產品在列表中的位置
+        const getCurrentProductIndex = productList.findIndex(
+          (product) => product._id === getCartItem?.productId,
+        );
+        const getTotalStock = productList[getCurrentProductIndex].totalStock;
+
+        console.log(getCurrentProductIndex, getTotalStock);
+
+        if (indexOfCurrentCartItem > -1) {
+          const getQuantity = getCartItems[indexOfCurrentCartItem].quantity;
+          if (getQuantity + 1 > getTotalStock) {
+            toast({
+              title: `Only ${getQuantity} quantity can be added for this item`,
+              variant: "destructive",
+            });
+
+            return;
+          }
+        }
+      }
+    }
+
     dispatch(
       updateCartQuantity({
         userId: user?.id,
